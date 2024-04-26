d96fdc2428d470f5df2b398604851f871c5eb198
Rahul Kumar <kumarrahul46700@gmail.com>
2024-04-26 23:45:34 +0530

    changes accepted from kuldeep's work ,

--- src/components/Toaster/APToaster.js
+++ src/components/Toaster/APToaster.js
@@ -8,12 +8,7 @@ const APToaster = forwardRef(({ title, x, y, type }, ref) => {
   useImperativeHandle(
     ref,
     () => ({
-<<<<<<< HEAD
-      showToaster: (props) => {
-        setOpen(true)
-=======
       showToast: (props) => {
->>>>>>> a00ddcd325746f24a13812e45d1e02738d5ad9af
         setmessageData(props);
         setOpen(true);
       },
@@ -27,15 +22,9 @@ const APToaster = forwardRef(({ title, x, y, type }, ref) => {
 
   return (
     <div>
-<<<<<<< HEAD
-      <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} anchorOrigin={messageData?.position ? { ...messageData?.position } : { vertical: 'top', horizontal: 'center' }}>
-        <Alert onClose={handleClose}  severity={messageData.messageType} variant="filled" sx={{ width: '400px',boxShadow:3, borderRadius: '30px' }}>
-          {messageData.message}
-=======
       <Snackbar open={open} autoHideDuration={2000} onClose={handleClose} anchorOrigin={messageData?.position ? { ...messageData?.position } : { vertical: 'top', horizontal: 'center' }}>
         <Alert onClose={handleClose} severity={messageData.messageType || 'success'} variant="filled" sx={{ width: '400px', boxShadow: 3, borderRadius: '30px' }}>
           {messageData?.messageText || ''}
->>>>>>> a00ddcd325746f24a13812e45d1e02738d5ad9af
         </Alert>
       </Snackbar>
     </div>
